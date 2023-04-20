c8bc03d9c2c0e75b7386f873e49ee79a8980c8f0
Ryan Canton <ryancanton999@gmail.com>
2023-04-20 13:22:16 -0600

    Feat: make schema

---
+++ db/migrate/20230420192004_create_subscriptions.rb
@@ -0,0 +1,14 @@
+class CreateSubscriptions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :subscriptions do |t|
+      t.string :title
+      t.string :price
+      t.boolean :status
+      t.integer :frequency
+      t.references :customer, null: false, foreign_key: true
+      t.references :tea, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
